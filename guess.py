e6b01c36efc5f9082e7eab3479267f4539b3c0de
Jenifer972006 <jeni09072006@gmail.com>
2024-11-18 21:32:42 +0530

    Update guess.py

--- guess.py
+++ guess.py
@@ -2,18 +2,11 @@ import streamlit as st
 import random
 
 st.title("Number Guessing Game")
-
-
-if 'random_number' not in st.session_state:
-    st.session_state.random_number = random.randint(1, 100)
-
-if 'attempts' not in st.session_state:
-    st.session_state.attempts = 0
-
-st.write("Guess the number between 1 and 100")
-         
-user_guess = st.number_input("Enter your guess:", min_value=1, max_value=100, step=1, key='guess')
-
+st.write("iam thinking number between 1 to 100")
+if 'number' not in st.session_state:
+    st.session_state.number = random.randint(1, 100)
+    st.session_state.attempts=0
+user_guess = st.number_input("Enter your guess:", min_value=1, max_value=100, step=1)
 if st.button("Submit Guess"):
     st.session_state.attempts += 1
     if user_guess < st.session_state.random_number:
@@ -22,6 +15,8 @@ if st.button("Submit Guess"):
         st.write("Too high! Try again.")
     else:
         st.write(f"Congratulations! You've guessed the number in {st.session_state.attempts} attempts.")
-        # Reset the game
-        st.session_state.random_number = random.randint(1, 100)
+        if st.button("play again"):
+        st.session_state.number = random.randint(1, 100)
         st.session_state.attempts = 0
+if st.session_state.attempt>0:
+    st.write(f"attempts:{st.session_state.attempts}")
